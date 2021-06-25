b62fe7b9afb96d62cc5f5e189c68eafe0763ad82
emilie.canches <emilie.canches@berger-levrault.com>
2021-06-25 11:41:09 +0200

    SpringBoot

---
+++ server/src/test/java/game/sixquiprends/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package game.sixquiprends.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
